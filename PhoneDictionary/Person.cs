cd30153e278632ec43ee0c4cb5e998bde127810e
Anton <anton.mileschko@gmail.com>
2022-03-30 16:54:22 +0700

    Добавьте файлы проекта.

---
+++ PhoneDictionary/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDictionary
+{
+    internal class Person
+    {
+        private string name;
+        private string secondName;
+
+        public Person(string name, string secondName)
+        {
+            this.name = name;
+            this.secondName = secondName;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + secondName;
+        }
+
+        public string Name
+        {
+            set => name = value;
+            get => name;
+        }
+
+        public string SecondName
+        {
+            get => secondName;
+        }
+    }
+}
---
+++ PhoneDictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PhoneDictionary;
+
+IndexArray arr = new IndexArray(new PhoneRecord[]
+{
+    new PhoneRecord("Anton", "Mileshko", "12345667"),
+    new PhoneRecord("Petr", "Petrov", "12345667"),
+    new PhoneRecord("Maria", "Kim", "12345667"),
+    new PhoneRecord("Alex", "Smolyar", "12345667")
+});
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i].ToString());
+}
+
+Console.WriteLine();
+
+arr.Sort();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i].ToString());
+}
+
+Person p1 = new Person("A", "B");
+Person p2 = p1;
+
+Console.WriteLine($"{p1} {p2}");
+
+p1.Name = "C";
+
+Console.WriteLine($"{p1} {p2}");
